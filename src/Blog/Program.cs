b68f32dea4fc88bb3ebf4e6bb2d1d252484f9a98
caleb ebie <chukwuma@gmail.com>
2021-09-05 01:41:02 +0100

    initial commit

---
+++ src/Blog/Program.cs
@@ -0,0 +1,17 @@
+using Blog;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+// read database configuration (database provider + database connection) from environment variables
+var config = new ConfigurationBuilder()
+    .AddEnvironmentVariables()
+    .Build();
+
+var host = new WebHostBuilder()
+    .UseConfiguration(config)
+    .UseKestrel()
+    .UseUrls($"http://localhost:5000")
+    .UseStartup<Startup>()
+    .Build();
+
+await host.RunAsync();
